76ded54155150380d52d78f686c6ea4a4ed85e4e
JavidAlakbarov <alakbarovjavid07@gmail.com>
2022-12-27 15:36:28 +0400

    fio

---
+++ ff.js
@@ -0,0 +1,30 @@
+
+
+  // $(document).ready(function () {
+  //   $(".slider-florist").slick({
+  //     prevArrow: '<i class="fas fa-long-arrow-alt-left"></i>',
+  //     nextArrow: '<i class="fas fa-long-arrow-alt-right"></i>',
+      
+  //   });
+  // });
+
+  
+
+/* Script show filter menu for product */
+
+// let filterProductShow = document.getElementById("filter-product")
+// filterProductShow.addEventListener("mouseenter", () => {
+//   document.getElementById("filter").style.display = "block";
+// });
+// filterProductShow.addEventListener("mouseleave", () => {
+//   document.getElementById("filter").style.display = "none";
+// });
+// let filterMenuShow = document.getElementById("filter")
+// filterMenuShow.addEventListener("mouseenter", () => {
+//   document.getElementById("filter").style.display = "block";
+// });
+// filterMenuShow.addEventListener("mouseleave", () => {
+//   document.getElementById("filter").style.display = "none";
+// });
+
+/*Script show filter menu for product son*/
